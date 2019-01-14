f6c4d6ebfa28642af537b5a6502a4f9bbdbf028d
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2019-01-13 21:35:20 -0300

    Starting the mine solver implementation

---
+++ minesolver.pl
@@ -0,0 +1,4 @@
+%passo 1 - pegar o valor de todos os oito vizinhos de cada campo com numero
+%passo 2 - ver a probabilidade de cada vizinhos
+%passo 3 - escolher a maior probabilidade
+%passo 3 - selecionar a jogada e repetir
